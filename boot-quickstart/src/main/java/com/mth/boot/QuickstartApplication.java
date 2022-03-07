3a9f8288160914437089d82d2fe704790fe22715
PatrickStarM <53247178PatrickStarM@users.noreply.github.com>
2022-03-07 15:22:06 +0800

    springboot-learning学习项目初始化

---
+++ boot-quickstart/src/main/java/com/mth/boot/QuickstartApplication.java
@@ -0,0 +1,19 @@
+package com.mth.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ProjectName: springboot-learning
+ * @Author: mth
+ * @Description:
+ * @Date: 2022/3/7
+ * @Version: 1.0
+ **/
+@SpringBootApplication
+public class QuickstartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuickstartApplication.class, args);
+    }
+
+}
---
+++ boot-quickstart/src/main/java/com/mth/boot/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.mth.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ProjectName: springboot-learning
+ * @Author: mth
+ * @Description:
+ * @Date: 2022/3/7
+ * @Version: 1.0
+ **/
+@RestController
+public class HelloController {
+    @GetMapping(value = "/hello")
+    public String getHello() {
+        return "hello world";
+    }
+}
